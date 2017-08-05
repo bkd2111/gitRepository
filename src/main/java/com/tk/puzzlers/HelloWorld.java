24aa8138435e4e35452afa24bfbfa9e364d6c0c6
bkd2111 <30566559+bkd2111@users.noreply.github.com>
2017-08-05 08:50:09 +0530

    Summary1
    Description1

---
+++ src/main/java/com/tk/puzzlers/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package main.java.com.tk.puzzlers;
+
+/**
+ * @author bkd2111
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
