8bbd4768beff6ecd0fc5fee64dd0c01f32020b6b
bkd2111 <30566559+bkd2111@users.noreply.github.com>
2017-08-05 09:05:35 +0530

    TestCommit1 : Added a class Arithmetic Operation

---
+++ src/main/java/com/tk/puzzlers/ArithmeticOperation.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package main.java.com.tk.puzzlers;
+
+/**
+ * @author bkd2111
+ *
+ */
+public class ArithmeticOperation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int number1 = 9;
+		int number2 = 13;
+		System.out.println(number1+number2);
+	}
+
+}
